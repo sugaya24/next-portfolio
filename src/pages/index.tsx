b30c8e68e0885a82de6ef9ceef9a4aed71fd8b41
YukiSugaya <sugaya324@gmail.com>
2021-12-07 08:00:22 +0900

    test: init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,75 +1,47 @@
 import Head from 'next/head';
-import Image from 'next/image';
+// import Image from 'next/image';
+import { createClient } from 'contentful';
 
 import styles from '@/styles/Home.module.css';
+import { IBlogPost, IBlogPostFields } from '@types/generated/contentful';
 
-export default function Home() {
+type Props = {
+  posts: IBlogPost[];
+};
+
+export async function getStaticProps() {
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID || ``,
+    accessToken: process.env.CONTENTFUL_DELIVERY_TOKEN || ``,
+  });
+  const res = await client.getEntries<IBlogPostFields>({
+    content_type: `blogPost`,
+  });
+
+  return { props: { posts: res.items } };
+}
+
+const Home: React.FC<Props> = ({ posts }) => {
   return (
     <div className={styles.container}>
       <Head>
-        <title>TypeScript starter for Next.js</title>
-        <meta
-          name="description"
-          content="TypeScript starter for Next.js that includes all you need to build amazing apps"
-        />
+        <title>Yuki Sugaya Portfolio</title>
+        <meta name="description" content="Yuki Sugaya Portfolio" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{` `}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <p className={styles.description}>This is not an official starter!</p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=typescript-nextjs-starter"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+      <main>
+        <h1>Yuki Sugaya</h1>
+        {posts.map((post: IBlogPost, index) => (
+          <div key={index}>
+            <h2>
+              {index + 1}: {post.fields.title}
+            </h2>
+          </div>
+        ))}
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=typescript-nextjs-starter"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{` `}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   );
-}
+};
+
+export default Home;
